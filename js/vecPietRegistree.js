31e1e9edf95ce7ce73eaf153a1ce871fddb8f329
krinta <70098878+krinta@users.noreply.github.com>
2020-11-23 19:32:00 +0200

    Jauns js fails tikai iesūtīt datus

---
+++ js/vecPietRegistree.js
@@ -0,0 +1,42 @@
+function vecPietRegistree(){
+    var vecVards=document.getElementById("vec_vards").value;
+    var vecUzvards=document.getElementById("vec_uzvards").value;
+    var vecEpasts=document.getElementById("vec_epasts").value;
+    var vecTelefons=document.getElementById("vec_telefons").value;
+    var dzimums=document.getElementById("berns").value;
+    var skVards=document.getElementById("sk_vards").value;
+    var skUzvards=document.getElementById("sk_uzvards").value;
+    var skSkola=document.getElementById("sk_skola").value;
+    var skKlase=document.getElementById("sk_klase").value;
+    var skPerskods=document.getElementById("sk_perskods").value;
+    var skAdrese=document.getElementById("sk_adrese").value;
+    var skEpasts=document.getElementById("sk_epasts").value;
+    var skTelefons=document.getElementById("sk_telefons").value;
+    var noteikumi=document.getElementById("not_ja").value;
+    if(vecVards==""||vecUzvards==""||vecTelefons==""||vecEpasts==""||skVards==""||skUzvards==""||skSkola==""||skKlase==""||skPerskods==""||skAdrese==""||skEpasts==""||skTelefons=="") alert("Visiem laukiem jābūt aizpildītājiem");
+    else{
+    var dict={vecVards:vecVards, vecUzvards:vecUzvards, vecEpasts:vecEpasts, vecTelefons:vecTelefons,
+        dzimums:dzimums, skVards:skVards, skUzvards:skUzvards, skSkola:skSkola, skKlase:skKlase,
+        skPerskods:skPerskods, skAdrese:skAdrese, skEpasts:skEpasts, skTelefons:skTelefons};
+        
+        jsonData = JSON.stringify(dict);
+        console.log(jsonData);
+        download(jsonData, 'vecakuPieteikumsSuutiit.txt', 'text/plain');
+
+    fetch('https://kalviskincis.github.io/interesu-izglitibas-IS/views/vecakuPieteikumsSuutiit.txt', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify(dict)
+    })    
+    .then(res => res.json())
+    .then(data => document.getElementById("zinojumi").innerHTML = "Dati saglabāti")
+   }
+}
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
