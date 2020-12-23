2607bcdb079b9ebe8597838e2ed70df3f670ba30
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2020-12-23 18:30:57 +0200

    Mainīta dzēst/labot saskarne pulciņu sarakstā. Funkcionalitātes vēl nav.

--- js/lasaPulcinuSarakstu.js
+++ js/lasaPulcinuSarakstu.js
@@ -1,9 +1,18 @@
+function labotPulcinu(pulcins) {
+// funkcija nolasīs padotā pulciņa laukus un atvērs pulcina_info.html skatu ar datiem
+}
+
+function dzestPulcinu(pulcins) {
+// funkcija izdzēsīs ierakstu un vēlreiz nolasīs datus tabulā
+// varbūt tad fetch jāliek kā funkcija.
+}
+
 function ieliktTabula(visiPulcini) {
     let tabulasBody = document.getElementById("tbody");
     let rinda;
     let laiks="";
     for (let i = 0; i < visiPulcini.length; i++) {
-        rinda = "<tr><td><input type=\"checkbox\" />    &nbsp;   </td><td>";   
+        rinda = "<tr><td><big><a href=\"javascript:\" onclick=\"labotPulcinu(visiPulcini[i])\" data-toggle=\"tooltip\" title=\"Labot\" >✎</a> &nbsp; <a href=\"javascript:\" onclick=\"dzestPulcinu(visiPulcini[i])\" data-toggle=\"tooltip\" title=\"Dzēst\">✘</a> </big></td><td>";
         rinda += visiPulcini[i].joma + "</td><td>" +
             visiPulcini[i].nosaukums + "</td><td>" +
             visiPulcini[i].stunduSkaits + "</td><td>" +
@@ -28,3 +37,7 @@ fetch(' https://kalviskincis.github.io/interesu-izglitibas-IS/pulcini.json')
     .then(res => res.json())
     .then(data => { console.log(data[0].joma); ieliktTabula(data); })
 
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
