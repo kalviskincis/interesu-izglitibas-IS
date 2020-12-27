da9914f4c9190e8d2e7e0fd8bddf8f22b1f3e623
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2020-12-27 18:59:39 +0200

    test-dati starp lapām

--- js/lasaPulcinuSarakstu.js
+++ js/lasaPulcinuSarakstu.js
@@ -1,18 +1,21 @@
-function myFunction(x) {
+// strādā
+function myFunction(x) { 
     rinda = x.rowIndex-1;
   }
 
+// strādā
 function labotPulcinu() {    
     var i = document.getElementById(rinda).id;
     console.log(rinda);
     atvertPulcinu(pulcDati[rinda]);
 }
 
+// sk. pie katras rindas
 function atvertPulcinu(pulcins) {
     console.log(pulcins.joma, pulcins.nosaukums); // parāda
     var pulcinaInfoLapa = window.open("pulcina_info.html"); // atver
     pulcinaInfoLapa.onload = function() { // vairs nekas nenotiek
-        window.alert(pulcins.nosaukums);
+        window.alert("boo");
         pulcinaInfoLapa.forma.joma.value=pulcins.joma;
         pulcinaInfoLapa.forma.nosaukums.value=pulcins.nosaukums;
         pulcinaInfoLapa.skolotajs.value="Aaaa";
@@ -36,7 +39,6 @@ function ieliktTabula(visiPulcini) {
         iDati = i;      
         rinda = "<tr onclick=\"myFunction(this)\"><td id=\""+i+"\">" + (i+1);        
         rinda += "</td><td><input type=\"button\" class=\"btn btn-primary\" onclick=\"labotPulcinu()\" value=\"✎\" data-toggle=\"tooltip\" title=\"Labot\">&nbsp; <input type=\"button\" class=\"btn btn-primary\" value=\"✘\" onclick=\"dzestPulcinu(visiPulcini[i])\" data-toggle=\"tooltip\" title=\"Dzēst\"></td><td name=\"joma\" id=\"joma\">";        
-        console.log(rinda);
         // rinda = "<tr><td><big><a href=\"javascript:\" onclick=\"labotPulcinu(visiPulcini[i])\" data-toggle=\"tooltip\" title=\"Labot\" >✎</a> &nbsp; <a href=\"javascript:\" onclick=\"dzestPulcinu(visiPulcini[i])\" data-toggle=\"tooltip\" title=\"Dzēst\">✘</a> </big></td><td name=\"joma\" id=\"joma\">";
         rinda += visiPulcini[i].joma + "</td><td name=\"nosaukums\" id=\"nosaukums\">" +
             visiPulcini[i].nosaukums + "</td><td>" +
@@ -47,10 +49,8 @@ function ieliktTabula(visiPulcini) {
             visiPulcini[i].talrunis + "</td><td>" +
             visiPulcini[i].adrese + "</td><td>" +
             visiPulcini[i].maxAudzekni + "</td><td>";
-            for (let j=0; j<visiPulcini[i].laiks.length; j++) {
-                //console.log(visiPulcini[i].laiks[j].diena, visiPulcini[i].laiks[j].no, visiPulcini[i].laiks[j].lidz);
+            for (let j=0; j<visiPulcini[i].laiks.length; j++) {                
                 laiks += visiPulcini[i].laiks[j].diena + ": " + visiPulcini[i].laiks[j].no + "—" + visiPulcini[i].laiks[j].lidz + "<br>";
-                //console.log(laiks);
             }
             rinda += laiks + "</td></tr>";
         tabulasBody.innerHTML += rinda;
