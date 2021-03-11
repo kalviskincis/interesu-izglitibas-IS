9b71d869b8a39cfcbcc8094bd7e13fc0e5814058
OksanaIvanova373 <70010980+OksanaIvanova373@users.noreply.github.com>
2021-03-11 19:23:02 +0200

    Update registracija.js

--- static/js/registracija.js
+++ static/js/registracija.js
@@ -1,18 +1,25 @@
-
   function registracija(){
     var adrese=document.getElementById("Adrese").value;
     var epasts=document.getElementById("Epasts").value;
     var talrunis=document.getElementById("Talrunis").value;
     var specialists=document.getElementById("Specialists").value;
-    var klubs=document.getElementById("iicnosauk").value;
+    var klubs=document.getElementById("Nosaukums").value;
     if(adrese==""||epasts==""||talrunis==""||specialists=="") alert("Visiem laukiem jābūt aizpildītājiem");
     else{
     var dict={adrese:adrese, epasts:epasts, talrunis:talrunis, specialists:specialists, klubs:klubs};
     console.log(JSON.stringify(dict));
-    fetch('https://interesu-izglitibas-IS.nitobenito.repl.co/registret_IIC', {
+    fetch('https://interesu-izglitibas-IS.oksanaivanova1.repl.co/registret_IIC', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body:JSON.stringify(dict)
-    })
+      })
+      .then(atb=>atb.json())
+      .then(response => {
+        console.log(response);
+        document.getElementById("iicsaturs").style="display:none";
+        document.getElementById("statuss").innerHTML=response.statuss;
+        document.getElementById("pulcins").innerHTML=response.pulcins;
+              
+    });
    }
 }
