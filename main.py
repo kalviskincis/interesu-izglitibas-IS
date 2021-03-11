726736e4c3fc79b9ef77ae485085905769ae4b67
OksanaIvanova373 <70010980+OksanaIvanova373@users.noreply.github.com>
2021-03-11 19:21:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, json, request
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII']=False
-
 
 @app.route('/')
 def index():
@@ -12,17 +11,40 @@ def index():
 def skIzvele():
   return render_template('skolenu_izvelne.html')
 
-@app.route('/IIC')
-def iic():
-  return render_template('IIC.html')  
-  
-@app.route('/vecaku_pietekumus')
-def vPieteikums():
+@app.route('/vecaku_pieteikums')
+def vecPiet():
   return render_template('vecaku_pieteikums.html')
 
-@app.route('/registret_IIC')
+@app.route('/IIC')
+def centrs():
+  return render_template('IIC.html')
+
+@app.route('/registret_IIC',methods=['POST'])
 def regIIC():
-  return 1
+  print ("ParbaudeParbaude")
+  with open("dati/regi.json", "r", encoding='utf-8') as f:
+    dati = json.loads(f.read())
+  jaunsIIC= json.loads(request.data)
+  print ("Parbaude")
+  for d in dati:
+    if d["klubs"]==jaunsIIC["klubs"]:
+      #d["forma"]=render_template("forma.html")
+      atbilde={"statuss":"Šī IIC jau ir reģistrēta"}
+      atbilde["pulcins"]=render_template('pulcina_info.html')
+      return jsonify(atbilde)
+
+  dati.append(jaunsIIC)
+
+  with open("dati/regi.json", "w", encoding='utf-8') as f:
+    f.write(json.dumps(dati))
+
+  atbilde={"statuss":"IIC veiksmīgi piereģistrēta"}
+  atbilde["pulcins"]=render_template('pulcina_info.html')
+  return jsonify(atbilde)
+
+  atbilde={"statuss":"IIC veiksmīgi piereģistrēta"}
+  atbilde["pulcins"]=render_template('pulcina_info.html')
+  return jsonify(atbilde)
 
 if __name__ == "__main__":
-   app.run("0.0.0.0", debug=True)
+  app.run("0.0.0.0", debug=True)
